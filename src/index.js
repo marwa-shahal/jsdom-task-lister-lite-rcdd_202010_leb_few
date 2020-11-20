1daacd3bd47f088ba53b7e879013e6c552efa4ec
binding-event-8377 <marwa.al.shahal@gmail.com>
2020-11-20 21:14:52 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+const button=document.getElementById("create-task-form");
+button.addEventListener("submit",taskstring);
 });
+
+function taskstring (e) {
+  e.preventDefault();
+ const task=document.getElementById("new-task-description");
+ const st=document.createElement('li');
+ st.innerText= task.value;
+ document.getElementById('tasks').appendChild(st);
+}
